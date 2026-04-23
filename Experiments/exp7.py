9d4a1f6e88e8f8e22e7527245e8a14330072c4e8
REGIMENTio <tanmayrookie1234@gmail.com>
2026-04-23 14:04:55 +0530

    added exp 7 and fixed venv shell issue

---
+++ Experiments/exp7.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+
+# 1. Load dataset
+df = pd.read_csv("pulse_output.csv")
+
+print("Original Data:\n", df.head())
+
+# 2. Handle missing values
+df['Calories'] = df['Calories'].fillna(df['Calories'].mean())
+df['Date'] = df['Date'].ffill()
+
+# 3. Fix Date format
+df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+
+# 4. Remove duplicates
+df = df.drop_duplicates()
+
+# 5. Remove outliers (Duration > 200)
+df = df[df['Duration'] < 200]
+
+# 6. Feature Scaling
+scaler = StandardScaler()
+scaled_data = scaler.fit_transform(df[['Duration','Pulse','Maxpulse','Calories']])
+
+scaled_df = pd.DataFrame(
+    scaled_data,
+    columns=['Duration','Pulse','Maxpulse','Calories']
+)
+
+# 7. Visualization
+plt.figure(figsize=(6,4))
+plt.scatter(df['Duration'], df['Calories'])
+plt.xlabel("Duration")
+plt.ylabel("Calories")
+plt.title("Duration vs Calories")
+plt.show()
+
+# 8. Output
+print("\nCleaned Data:\n", df.head())
+print("\nScaled Data:\n", scaled_df.head())
+
+# 9. Extra (for better marks)
+print("\nDataset Info:\n")
+print(df.info())
+
+print("\nStatistical Summary:\n")
+print(df.describe())
